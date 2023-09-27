4b5b1b3e8728c3975b4435fac46e176eeae6c109
Ali Yavari <ali.yavari@meduniwien.ac.at>
2023-09-27 15:02:34 +0200

    The corrected version of functions (without using objects) have been added(utilfs).

--- oct_converter/readers/utilfs.py
+++ oct_converter/readers/utilfs.py
@@ -3,8 +3,6 @@ import eyepy
 from oct_converter.readers.BINReader import BINReader
 import h5py
 from tenpy.tools import hdf5_io # should be installed pip install physics-tenpy
-import pickle
-import pandas as pd
 
 '''
 *1- Loading, creating and returting a .eye object  from a .bin path 
@@ -37,56 +35,36 @@ def save_bin2eye(src_filepath, des_filepath, header_size=0, num_slices=2044, wid
     eye_volume.save(des_filepath)
 
 
-import dill
-protocol = 3
 '''
-*3- Saving a .eye object in a .bin file 
-eye_object: the .eye eyevolume object  
-bin_filepath: Name and path of the desired .bin file    
-'''  
-def save_eye2bin_object(eye_object, bin_filepath):
-    try:
-        # Serialize the object to bytes using pickle
-        serialized_eye = dill.dumps(eye_object, protocol=protocol)
-        
-        # Create a Pandas Series with the serialized object
-        eye_series = pd.DataFrame([serialized_eye])
-        
-        # Use pd.to_pickle to save the Series to a binary file
-        pd.to_pickle(eye_series, bin_filepath)
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-'''
-*4- Saving a .eye file to a .bin file 
+*3- Saving a .eye file to a .bin file 
 eye_filepath: Name and path of the .eye file    
 bin_filepath: Name and path of the desired .bin file    
 '''  
-def save_eye2bin_file(eye_filepath, bin_filepath):
+def save_eye2bin(eye_filepath, bin_filepath):
     try:
         eye_object = eyepy.KnotEyeVolume.load(eye_filepath)
         
-        # Serialize the object to bytes using pickle
-        serialized_eye = dill.dumps(eye_object, protocol=protocol)
-        
-        # Create a Pandas Series with the serialized object
-        eye_series = pd.DataFrame([serialized_eye])
-        
-        # Use pd.to_pickle to save the Series to a binary file
-        pd.to_pickle(eye_series, bin_filepath)
+        parts = bin_filepath.rsplit('.', 1)
+        if len(parts) == 2:
+            characters_before_dot = parts[0]
+        else:
+            characters_before_dot = bin_filepath
 
+        data_path = characters_before_dot + '_structure.bin'
+        data_part = eye_object.data
+        with open(data_path,'wb') as f:
+            f.write(data_part)
+            
     except Exception as e:
         print(f"Error: {e}")
 
 
 '''
-*5- Saving a .eye file to a .hdf5 file
+*4- Saving a .eye file to a .hdf5 file
 eye_filepath:Name and path of the .eye file      
 filepath: Name and path of the desired .hdf5 file    
 '''      
-def save_eye2hdf5_file(eye_filepath, hdf5_filepath):
+def save_eye2hdf5(eye_filepath, hdf5_filepath):
     try:
         eye_object = eyepy.KnotEyeVolume.load(eye_filepath)
         # Open the HDF5 file
@@ -97,26 +75,12 @@ def save_eye2hdf5_file(eye_filepath, hdf5_filepath):
         print(f"Error: {e}")
 
 
-'''
-*6- Saving a .eye object in a .hdf5 file
-hdf5_filepath: Name and path of the desired .hdf5 file    
-eye_object: the .eye eyevolume object  
-'''      
-def save_eye2hdf5_object(eye_object, hdf5_filepath):
-    try:        
-        # Open the HDF5 file
-        with h5py.File(hdf5_filepath, 'w') as f:
-            # Save the object to the HDF5 file
-            hdf5_io.save_to_hdf5(f, eye_object)
-    except Exception as e:
-        print(f"Error: {e}")
-
 
 '''
-*7- Loading and returning a .eye object from a .hdf5 file
+*5- Loading and returning a .eye object from a .hdf5 file
 hdf5_filepath: Name and path of the .hdf5 file    
 '''      
-def load_eye_from_hdf5_object(hdf5_filepath):
+def load_eye_from_hdf5(hdf5_filepath):
     try:
         # Open the HDF5 file
         with h5py.File(hdf5_filepath, 'r') as f:
@@ -126,3 +90,4 @@ def load_eye_from_hdf5_object(hdf5_filepath):
         print(f"Error: {e}")
     
     return loaded_eye_object
+
